b769d5db3d3ff74280a9c5994bd4fdb96fa2754a
Дмитрий Струнгарь <dmitry.strungar@gmail.com>
2026-05-13 15:26:28 +0300

    Update 1_revenue_by_category.sql

--- sql/1_revenue_by_category.sql
+++ sql/1_revenue_by_category.sql
@@ -1,1 +1,9 @@
-
+select 
+    Category,
+    round(sum(Amount), 2) as total_revenue,
+    count() as orders_count
+from amazon_analytics.amazon_sales
+where Status = 'Shipped'
+group by Category
+order by total_revenue desc
+limit 10
