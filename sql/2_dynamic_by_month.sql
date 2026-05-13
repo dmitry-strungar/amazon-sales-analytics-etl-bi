d0a9608b51486db96f877aa70d026d3c737665f6
Дмитрий Струнгарь <dmitry.strungar@gmail.com>
2026-05-13 15:27:14 +0300

    Update 2_dynamic_by_month.sql

--- sql/2_dynamic_by_month.sql
+++ sql/2_dynamic_by_month.sql
@@ -1,1 +1,8 @@
-
+select 
+    toStartOfMonth(toDate(concat('20', substring(Date, 7, 2), '-', substring(Date, 1, 2), '-', substring(Date, 4, 2)))) as month,
+    round(sum(Amount), 2) as revenue,
+    count() as orders
+from amazon_analytics.amazon_sales
+where Status = 'Shipped'
+group by month
+order by month
