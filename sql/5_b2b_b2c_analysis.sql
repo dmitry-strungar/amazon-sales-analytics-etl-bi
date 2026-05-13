9edc083446489b0b3f06765437e1f1f2f2ade6ea
Дмитрий Струнгарь <dmitry.strungar@gmail.com>
2026-05-13 15:40:12 +0300

    Update and rename 6_b2b_b2c_analysis.sql to 5_b2b_b2c_analysis.sql

---
+++ sql/5_b2b_b2c_analysis.sql
@@ -0,0 +1,10 @@
+select 
+    case when B2B = true then 'B2B' else 'B2C' end as sale_type,
+    count() as orders_count,
+    round(sum(Amount), 2) as total_revenue,
+    round(avg(Amount), 2) as avg_order_value,
+    count(distinct 'ship-state') as states_covered
+from amazon_analytics.amazon_sales
+where Status = 'Shipped'
+group by B2B
+
